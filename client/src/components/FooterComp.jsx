d034b26dec85274062c0b186dad4c1602a08ce4d
alimassidik210 <alim.assidik210@gmail.com>
2024-01-21 18:00:37 +0700

    create and add the footer component

---
+++ client/src/components/FooterComp.jsx
@@ -0,0 +1,82 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsDribbble,
+} from "react-icons/bs";
+import React from "react";
+
+export default function FooterComp() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-2">
+          <div className="mt-5">
+            <Link
+              to={"/"}
+              className="self-center font-semibold text-lg sm:text-xl dark:text-white whitespace-nowrap"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">
+                Liqooo's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Rumaysho.com
+                </Footer.Link>
+                <Footer.Link href="/about">Liqooo's Blog</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.github.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github.com
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Term &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Liqooo's blog"
+            year={new Date().getFullYear()}
+          />
+        </div>
+        <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+          <Footer.Icon href="#" icon={BsFacebook} />
+          <Footer.Icon href="#" icon={BsInstagram} />
+          <Footer.Icon href="#" icon={BsTwitter} />
+          <Footer.Icon href="#" icon={BsGithub} />
+          <Footer.Icon href="#" icon={BsDribbble} />
+        </div>
+      </div>
+    </Footer>
+  );
+}
