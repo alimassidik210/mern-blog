dbe02e76934e216e0bdbfda38aaf795f2a70dee1
alimassidik210 <alim.assidik210@gmail.com>
2024-01-21 14:59:19 +0700

    create sign up ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
         className="self-center font-semibold text-sm sm:text-xl dark:text-white whitespace-nowrap"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">
-          Liqo's
+          Liqooo's
         </span>
         Blog
       </Link>
