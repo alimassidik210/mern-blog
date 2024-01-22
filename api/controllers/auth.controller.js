b2df9692357739cfccd8ddc7cad2740ba8e28c5e
alimassidik210 <alim.assidik210@gmail.com>
2024-01-22 13:06:27 +0700

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,6 +28,41 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === " " || password === "") {
+    next(errorHandler(400, "Internal Server Error!"));
+  }
+
+  try {
+    const valideUser = await User.findOne({ email });
+
+    if (!valideUser) {
+      next(errorHandler(400, "User not Found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, valideUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+
+    const token = jwt.sign({ id: valideUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = valideUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- client/src/components/FooterComp.jsx
+++ client/src/components/FooterComp.jsx
@@ -12,7 +12,7 @@ import React from "react";
 export default function FooterComp() {
   return (
     <Footer container className="border border-t-8 border-teal-500">
-      <div className="w-full max-w-7xl mx-auto">
+      <div className="w-full max-w-7xl mx-auto ">
         <div className="grid w-full justify-between sm:flex md:grid-cols-2">
           <div className="mt-5">
             <Link
@@ -68,13 +68,13 @@ export default function FooterComp() {
             by="Liqooo's blog"
             year={new Date().getFullYear()}
           />
-        </div>
-        <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-          <Footer.Icon href="#" icon={BsFacebook} />
-          <Footer.Icon href="#" icon={BsInstagram} />
-          <Footer.Icon href="#" icon={BsTwitter} />
-          <Footer.Icon href="#" icon={BsGithub} />
-          <Footer.Icon href="#" icon={BsDribbble} />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
         </div>
       </div>
     </Footer>
