a5c1c037e396564fa084ac0c33f37121e6f0815f
alimassidik210 <alim.assidik210@gmail.com>
2024-02-08 11:06:34 +0700

    add comments section to the admin dashboard

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -4,6 +4,7 @@ import {
   HiArrowSmRight,
   HiDocumentText,
   HiOutlineUserGroup,
+  HiAnnotation,
 } from "react-icons/hi";
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
@@ -73,6 +74,17 @@ export default function DashSideBar() {
                 as="div"
               >
                 Users
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser.isAdmin && (
+            <Link to={"/dashboard?tab=comments"}>
+              <Sidebar.Item
+                active={tab === "comments"}
+                icon={HiAnnotation}
+                as="div"
+              >
+                Comments
               </Sidebar.Item>
             </Link>
           )}
